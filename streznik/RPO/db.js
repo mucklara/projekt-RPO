b74e88d1e508fe6dc7df57fde0bf195753cc5644
mucklara <146102334+mucklara@users.noreply.github.com>
2024-12-03 16:34:37 +0100

    Update db.js

--- streznik/RPO/db.js
+++ streznik/RPO/db.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2/promise');
 // Create a connection pool
 const db = mysql.createPool({
     host: 'localhost',        // MySQL server
-    user: 'root',             // MySQL username (replace if different)
-    password: 'Lip6.Ves9l00ps', // MySQL password (replace with yours)
-    database: 'LanguageLearning', // Replace with your database name
-    port: 3306                // MySQL default port
+    user: 'root',             // MySQL username 
+    password: '', // MySQL password (dopolnite s svojo)
+    database: 'LanguageLearning', // Nazadnje sem delala s to
+    port: 3306                // MySQL port
 });
 
 module.exports = db;
